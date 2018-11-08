e5fd0b8baec920a7dd5d6bd837190967fc0b5cc8
NikkiRufiansya <nikkirufiansya88@gmail.com>
2018-11-08 14:02:35 +0700

    push ui desgin list view

---
+++ UI LISTVIEW/app/src/main/java/com/example/benerin/ui_listview/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.benerin.ui_listview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button absen ,jadwal,nilai,belajar,berita,informasi;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        absen = (Button) findViewById(R.id.absen);
+        absen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Absen.class);
+                startActivity(intent);
+            }
+        });
+        jadwal = (Button) findViewById(R.id.jadwal);
+        jadwal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Jadwal.class);
+                startActivity(intent);
+            }
+        });
+        nilai = (Button) findViewById(R.id.nilai);
+        nilai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Nilai.class);
+                startActivity(intent);
+            }
+        });
+        belajar = (Button) findViewById(R.id.belajar);
+        belajar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Belajar.class);
+                startActivity(intent);
+            }
+        });
+        berita = (Button) findViewById(R.id.berita);
+        berita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Berita.class);
+                startActivity(intent);
+            }
+        });
+        informasi = (Button) findViewById(R.id.informasi);
+        informasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Informasi.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
