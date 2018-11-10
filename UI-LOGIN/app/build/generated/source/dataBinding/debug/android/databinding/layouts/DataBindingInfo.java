380ca97cf85891c8dd1251c53126c6d83bb097e0
NikkiRufiansya <nikkirufiansya88@gmail.com>
2018-11-10 12:57:01 +0700

    UI-LOGIN

---
+++ UI-LOGIN/app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="8f814c6c-98c6-42ed-bb94-2a31ce94b0cf")
+public class DataBindingInfo {}
